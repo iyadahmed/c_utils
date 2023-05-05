cb05469634e7ac9af4871c347c0edf80093ce59d
Iyad Ahmed <iyadahmed430@gmail.com>
2023-05-05 05:30:52 +0300

    Add helper macro for typedefing enums

--- common.h
+++ common.h
@@ -7,6 +7,10 @@
     typedef struct struct_name struct_name; \
     struct struct_name
 
+#define ENUM(enum_name)               \
+    typedef enum enum_name enum_name; \
+    enum enum_name
+
 // Not a good pattern to print and error and exit like that but let's do it for now
 void bail(const char *msg)
 {
