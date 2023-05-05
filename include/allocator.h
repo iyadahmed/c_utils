d3af793868711ce8bb48b7a070fba2892059bb05
Iyad Ahmed <iyadahmed430@gmail.com>
2023-05-06 02:10:37 +0300

    Format allocator.h

--- include/allocator.h
+++ include/allocator.h
@@ -8,38 +8,38 @@
 
 STRUCT(allocator_t)
 {
-    void *mem;
-    size_t cap;
-    size_t current_index;
+	void* mem;
+	size_t cap;
+	size_t current_index;
 };
 
 allocator_t create_allocator(size_t cap)
 {
-    void *mem = malloc(cap);
-    if (mem == NULL)
-    {
-        bail("Failed to allocate memory");
-    }
-    return {mem, cap, 0};
+	void* mem = malloc(cap);
+	if (mem == NULL)
+	{
+		bail("Failed to allocate memory");
+	}
+	return { mem, cap, 0 };
 }
 
-size_t allocate(allocator_t *allocator, size_t num_bytes)
+size_t allocate(allocator_t* allocator, size_t num_bytes)
 {
-    if (num_bytes == 0)
-    {
-        bail("It does not make any sense to allocate 0 bytes");
-    }
-    if ((allocator->current_index + num_bytes) > allocator->cap)
-    {
-        allocator->cap = (allocator->cap + num_bytes) * 2;
-        allocator->mem = realloc(allocator->mem, allocator->cap);
-        if (allocator->mem == NULL)
-        {
-            bail("Failed to re-allocate memory");
-        }
-    }
+	if (num_bytes == 0)
+	{
+		bail("It does not make any sense to allocate 0 bytes");
+	}
+	if ((allocator->current_index + num_bytes) > allocator->cap)
+	{
+		allocator->cap = (allocator->cap + num_bytes) * 2;
+		allocator->mem = realloc(allocator->mem, allocator->cap);
+		if (allocator->mem == NULL)
+		{
+			bail("Failed to re-allocate memory");
+		}
+	}
 
-    size_t result = allocator->current_index;
-    allocator->current_index += num_bytes;
-    return result;
+	size_t result = allocator->current_index;
+	allocator->current_index += num_bytes;
+	return result;
 }
