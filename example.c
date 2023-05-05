e8ecefb0fb797274482b7cf58baef53f72a436be
Iyad Ahmed <iyadahmed430@gmail.com>
2023-05-05 19:38:06 +0300

    Write function to free hash maps and format file using Visual Studio's default formatting rules and detect leaks using CRT

--- example.c
+++ example.c
@@ -1,48 +1,60 @@
+#ifdef _MSC_VER
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif
+
 #include <stdio.h>
 
 #include "utf8.h"
 #include "map.h"
 #include "list.h"
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    linked_list_item_t *last = NULL;
+	linked_list_item_t* last = NULL;
 
-    map_t set = create_map(100);
+	map_t set = create_map(100);
 
-    for (int i = 0; i < 100; i++)
-    {
-        linked_list_item_t *new_item = malloc(sizeof(linked_list_item_t));
-        int *data = malloc(sizeof(int));
-        *data = i;
-        new_item->data = data;
-        APPEND_TO_LINKED_LIST(&last, new_item);
+	for (int i = 0; i < 100; i++)
+	{
+		linked_list_item_t* new_item = malloc(sizeof(linked_list_item_t));
+		int* data = malloc(sizeof(int));
+		*data = i;
+		new_item->data = data;
+		APPEND_TO_LINKED_LIST(&last, new_item);
 
-        int *key = malloc(sizeof(int));
-        *key = i;
+		int* key = malloc(sizeof(int));
+		*key = i;
 
-        int *value = malloc(sizeof(int));
-        *value = 2 * i - 1;
+		int* value = malloc(sizeof(int));
+		*value = 2 * i - 1;
 
-        insert_to_map(&set, key, sizeof(int), value);
-    }
+		insert_to_map(&set, key, sizeof(int), value);
+	}
 
-    int foo = 1337;
-    INSERT_TO_MAP_BY_STRING_LITERAL_KEY(&set, "IYAD", &foo);
+	int foo = 1337;
+	INSERT_TO_MAP_BY_STRING_LITERAL_KEY(&set, "IYAD", &foo);
 
-    while (last != NULL)
-    {
-        int key = *((int *)(last->data));
+	while (last != NULL)
+	{
+		int key = *((int*)(last->data));
 
-        int *value = get_from_map(&set, &key, sizeof(int));
+		int* value = get_from_map(&set, &key, sizeof(int));
 
-        printf("%d %d\n", key, *value);
+		printf("%d %d\n", key, *value);
 
-        last = last->prev;
-    }
+		last = last->prev;
+	}
 
-    int *value = GET_FROM_MAP_BY_STRING_LITERAL_KEY(&set, "IYAD");
-    printf("%d\n", *value);
+	int* value = GET_FROM_MAP_BY_STRING_LITERAL_KEY(&set, "IYAD");
+	printf("%d\n", *value);
 
-    return 0;
+	free_map(&set);
+
+#ifdef _MSC_VER
+	_CrtDumpMemoryLeaks();
+#endif
+
+	return 0;
 }
--- include/map.h
+++ include/map.h
@@ -10,93 +10,109 @@
 #include "siphash.h"
 
 // Arbitrary chosen key
-const uint8_t SIPHASH_KEY[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+const uint8_t SIPHASH_KEY[16] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
 STRUCT(map_bucket_item_t)
 {
-    uint64_t key_hash;
-    void *key;
-    size_t key_length;
-    void *value;
-    map_bucket_item_t *prev;
+	uint64_t key_hash;
+	void* key;
+	size_t key_length;
+	void* value;
+	map_bucket_item_t* prev;
 };
 
 STRUCT(map_t)
 {
-    map_bucket_item_t **buckets;
-    size_t num_buckets;
+	map_bucket_item_t** buckets;
+	size_t num_buckets;
 };
 
 map_t create_map(size_t num_buckets)
 {
-    if (num_buckets == 0)
-    {
-        bail("Does not make sense to create a set with 0 buckets");
-    }
-    map_bucket_item_t **buckets = calloc(num_buckets, sizeof(map_bucket_item_t *));
-    if (buckets == NULL)
-    {
-        bail("Failed to allocate buckets");
-    }
+	if (num_buckets == 0)
+	{
+		bail("Does not make sense to create a set with 0 buckets");
+	}
+	map_bucket_item_t** buckets = calloc(num_buckets, sizeof(map_bucket_item_t*));
+	if (buckets == NULL)
+	{
+		bail("Failed to allocate buckets");
+	}
 
-    return (map_t){buckets, num_buckets};
+	return (map_t) { buckets, num_buckets };
 }
 
-void insert_to_map(map_t *set, void *key, size_t key_length, void *value)
+void insert_to_map(map_t* set, void* key, size_t key_length, void* value)
 {
-    uint64_t key_hash;
-    siphash(key, key_length, SIPHASH_KEY, (uint8_t *)(&key_hash), sizeof(uint64_t));
-    size_t bucket_index = key_hash % (set->num_buckets);
+	uint64_t key_hash;
+	siphash(key, key_length, SIPHASH_KEY, (uint8_t*)(&key_hash), sizeof(uint64_t));
+	size_t bucket_index = key_hash % (set->num_buckets);
 
-    map_bucket_item_t *new_bucket_item = malloc(sizeof(map_bucket_item_t));
-    new_bucket_item->key = key;
-    new_bucket_item->key_hash = key_hash;
-    new_bucket_item->key_length = key_length;
-    new_bucket_item->value = value;
-    new_bucket_item->prev = NULL;
-    APPEND_TO_LINKED_LIST(set->buckets + bucket_index, new_bucket_item);
+	map_bucket_item_t* new_bucket_item = malloc(sizeof(map_bucket_item_t));
+	new_bucket_item->key = key;
+	new_bucket_item->key_hash = key_hash;
+	new_bucket_item->key_length = key_length;
+	new_bucket_item->value = value;
+	new_bucket_item->prev = NULL;
+	APPEND_TO_LINKED_LIST(set->buckets + bucket_index, new_bucket_item);
 }
 
 #define INSERT_TO_MAP_BY_STRING_LITERAL_KEY(set_ptr, string_literal, value_ptr) \
     insert_to_map(set_ptr, string_literal, sizeof(string_literal), value_ptr);
 
-bool match_key(map_bucket_item_t *item, void *key, uint64_t key_hash, size_t key_length)
+bool match_key(map_bucket_item_t* item, void* key, uint64_t key_hash, size_t key_length)
 {
-    if (item->key == key)
-    {
-        return true;
-    }
-    else if (item->key_hash == key_hash)
-    {
-        return true;
-    }
+	if (item->key == key)
+	{
+		return true;
+	}
+	else if (item->key_hash == key_hash)
+	{
+		return true;
+	}
 
-    if (item->key_length != key_length)
-    {
-        return false;
-    }
+	if (item->key_length != key_length)
+	{
+		return false;
+	}
 
-    return memcmp(item->key, key, item->key_length) == 0;
+	return memcmp(item->key, key, item->key_length) == 0;
 }
 
-void *get_from_map(map_t *set, void *key, size_t key_length)
+void* get_from_map(map_t* set, void* key, size_t key_length)
 {
-    uint64_t key_hash;
-    siphash(key, key_length, SIPHASH_KEY, (uint8_t *)(&key_hash), sizeof(uint64_t));
-    size_t bucket_index = key_hash % (set->num_buckets);
+	uint64_t key_hash;
+	siphash(key, key_length, SIPHASH_KEY, (uint8_t*)(&key_hash), sizeof(uint64_t));
+	size_t bucket_index = key_hash % (set->num_buckets);
 
-    map_bucket_item_t *last = set->buckets[bucket_index];
-    while (last != NULL)
-    {
-        if (match_key(last, key, key_hash, key_length))
-        {
-            return last->value;
-        }
+	map_bucket_item_t* last = set->buckets[bucket_index];
+	while (last != NULL)
+	{
+		if (match_key(last, key, key_hash, key_length))
+		{
+			return last->value;
+		}
 
-        last = last->prev;
-    }
-    return NULL;
+		last = last->prev;
+	}
+	return NULL;
 }
 
 #define GET_FROM_MAP_BY_STRING_LITERAL_KEY(set_ptr, string_literal) \
     get_from_map(set_ptr, string_literal, sizeof(string_literal));
+
+void free_map(map_t* set)
+{
+	for (size_t i = 0; i < set->num_buckets; i++)
+	{
+		map_bucket_item_t* last = set->buckets[i];
+		while (last != NULL)
+		{
+
+			map_bucket_item_t* prev = last->prev;
+			free(last);
+			last = prev;
+		}
+	}
+	free(set->buckets);
+}
